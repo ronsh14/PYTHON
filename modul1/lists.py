f5c1d65dafe6f60264c71afb330cced99039a508
ron shala <ron.shala17@gmail.com>
2025-11-08 14:33:15 +0100

    modul1

---
+++ modul1/lists.py
@@ -0,0 +1,29 @@
+to_do_list = ["math homework","clean the room","reasearch for history","fun"]
+
+print(to_do_list[0])
+print(to_do_list[1])
+print(to_do_list[2])
+print(to_do_list[3])
+
+
+shopping_list = ["apples" , "bananas",3,4.5]
+shopping_list.append("oranges")
+
+print(shopping_list)
+
+shopping_list.insert(2,"lemons")
+print(shopping_list)
+
+to_do_list.remove("reasearch for history")
+print(to_do_list)
+
+del to_do_list[2]
+print(to_do_list)
+
+to_do_list[0] = "physics homework"
+print(to_do_list)
+
+to_do_list[1] = 32
+print(to_do_list)
+
+
---
+++ modul1/variables.py
@@ -0,0 +1,24 @@
+name = "ronn"
+print(name)
+
+temperature = 98.6
+print(temperature)
+print(type(temperature))
+
+
+age = 30
+print("age is:",age)
+
+x = 5
+y = 3
+result = x + y
+print("rezultati eshte :" ,result)
+
+age = age +1
+print("age now is:", age)
+
+first_name = "john"
+last_name = "doe"
+full_name = first_name + " " + last_name
+
+print(full_name)
